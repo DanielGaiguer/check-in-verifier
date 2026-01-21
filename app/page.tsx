34e31e802bc113786583b549092953d62580bbb4
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-21 11:59:52 -0300

    fix bug prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-"use client";
+'use client'
 
 export default function Home() {
   return (
     <main className="p-4">
-      <h1 className="flex p-4 items-center justify-betweend">Bem-vindo ao Check-in Verifier!</h1>
+      <h1 className="flex items-center justify-between p-4">
+        Bem-vindo ao Check-in Verifier!
+      </h1>
       {/* <CheckinWizard /> Client Component */}
     </main>
-  );
+  )
 }
