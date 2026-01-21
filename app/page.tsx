513f78d89ddd84f0e69ca1e8f5b6ba59c803f475
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-21 11:57:22 -0300

    add prettier and eslint

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main className="p-4">
-      <h1 className="text-2xl font-bold">Bem-vindo ao Check-in Verifier!</h1>
+      <h1 className="flex p-4 items-center justify-betweend">Bem-vindo ao Check-in Verifier!</h1>
       {/* <CheckinWizard /> Client Component */}
     </main>
   );
