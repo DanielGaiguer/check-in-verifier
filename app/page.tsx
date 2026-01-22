b24a546ca4cef5a69077050e2e9eff6e1165a208
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-22 09:28:29 -0300

    req in the front

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-'use client'
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch(process.env.URL + "/api/checkins", {
+    //headers: { "Content-Type": "application/json" },
+    //body: JSON.stringify(data),
+  });
+
+  const data = await response.json()
+
+  console.log(data)
+
   return (
     <main className="p-4">
       <h1 className="flex items-center justify-between p-4">
         Bem-vindo ao Check-in Verifier!
+        {data[0].id}
       </h1>
       {/* <CheckinWizard /> Client Component */}
     </main>
