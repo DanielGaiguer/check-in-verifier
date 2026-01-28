a12dc999ea30558c1dd2f07c2773bc0d4a1822e9
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-01-28 15:55:36 -0300

    fix bug in style

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { TodayCheckin } from "@/components/checkins/TodayCheckin"
 
 export default async function Home() {
   return (
-    <main className="p-4 flex items-center flex-col">
+    <main className="flex flex-col items-center justify-center">
       <h1 className="flex items-center p-4 text-xl font-semibold tracking-tight">
         Bem-vindo ao Check-in Verifier!
         </h1>
--- src/components/checkins/TodayCheckin.tsx
+++ src/components/checkins/TodayCheckin.tsx
@@ -10,7 +10,6 @@ import { Button } from '../ui/button'
 import { AlertCircle } from 'lucide-react'
 import Link from 'next/link'
 import { getCheckinServer } from '@/services/checkins.server'
-import SelectDateForm from './selectDateForm'
 import CardCheckin from './cardCheckin'
 
 export async function TodayCheckin() {
@@ -21,8 +20,8 @@ export async function TodayCheckin() {
       {data.length ? (
         <>
           <h2>Check-in de Hoje: </h2>
-          <div className="flex w-[90%] flex-col items-center">
-            <div className="flex w-[75%] flex-row justify-between p-5">
+          <div className="flex w-full flex-col items-center">
+            <div className="flex w-full flex-col items-center justify-center p-4 lg:w-[45%]">
               <CardCheckin checkins={data} />
             </div>
           </div>
