9cda0a778e1df0a0dab619e16ad78ccf18037e88
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-02-03 11:46:25 -0300

    finally component Collapsible for issue

--- src/components/checkins/cardPlace.tsx
+++ src/components/checkins/cardPlace.tsx
@@ -7,8 +7,12 @@ import {
   ItemTitle,
 } from '@/components/ui/item'
 import { Checkbox } from '../ui/checkbox'
-import { useState } from 'react'
-import { Collapsible, CollapsibleContent } from '../ui/collapsible'
+import { useEffect, useState } from 'react'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '../ui/collapsible'
 import {
   Field,
   FieldDescription,
@@ -18,6 +22,14 @@ import {
   FieldSet,
 } from '../ui/field'
 import { FileUploadCircularProgress } from '../drop-files'
+import { Textarea } from '../ui/textarea'
+import { Button } from '../ui/button'
+import {
+  CheckCheckIcon,
+  CheckCircle,
+  CheckIcon,
+  ChevronDownIcon,
+} from 'lucide-react'
 
 interface PlaceProtocol {
   place: {
@@ -36,6 +48,9 @@ export const CardPlace = ({ place, issues }: PlaceProtocol) => {
   const [status, setStatus] = useState<'organized' | 'disorganized' | null>(
     null
   )
+
+  const [open, setOpen] = useState(false)
+
   return (
     <>
       <Item
@@ -45,7 +60,9 @@ export const CardPlace = ({ place, issues }: PlaceProtocol) => {
           status === 'organized'
             ? 'border-white bg-green-300'
             : status === 'disorganized'
-              ? 'mb-0 rounded-b-none border-2 border-b-0 bg-red-300'
+              ? open
+                ? 'mb-0 rounded-b-none border-2 border-b-0 bg-red-300'
+                : 'border-2 bg-red-300'
               : 'bg-gray-300'
         }`}
       >
@@ -54,7 +71,9 @@ export const CardPlace = ({ place, issues }: PlaceProtocol) => {
             {place.name}
           </ItemTitle>
           <ItemDescription className="text-gray-700">
-            Selecione se o local está organizado ou não
+            {!status && (
+              <span>Selecione se o local está organizado ou não</span>
+            )}
           </ItemDescription>
         </ItemContent>
 
@@ -73,21 +92,47 @@ export const CardPlace = ({ place, issues }: PlaceProtocol) => {
           <Checkbox
             id={`disorganized-${place.id}`}
             checked={status === 'disorganized'}
-            onCheckedChange={(checked) =>
+            onCheckedChange={(checked) => {
               setStatus(checked ? 'disorganized' : null)
-            }
+              setOpen(!!checked)
+            }}
           />
           <label htmlFor={`disorganized-${place.id}`}>Desorganizado</label>
+
+          {status === 'disorganized' && (
+            <Button
+              variant="ghost"
+              size="icon"
+              className="ml-1 h-6 w-6"
+              onClick={() => setOpen((prev) => !prev)}
+            >
+              <ChevronDownIcon
+                className={`transition-transform ${open ? 'rotate-180' : ''}`}
+              />
+            </Button>
+          )}
         </ItemActions>
+        {status === 'organized' && (
+          <Button
+            variant="ghost"
+            size="icon"
+            className="ml-1 h-6 w-6"
+            onClick={() => setOpen((prev) => !prev)}
+          >
+            <CheckCircle />
+          </Button>
+        )}
       </Item>
 
-      {/* Collapsible fora do Item, mas colado */}
-      <Collapsible open={status === 'disorganized'}>
-        <CollapsibleContent className="-mt-1 max-h-0 w-full overflow-hidden transition-all duration-500 ease-out data-[state=open]:max-h-96">
-          <div className="mt-0 w-full rounded-b-md border-2 border-t-0 border-black bg-red-300 p-2">
+      <Collapsible
+        open={open && status === 'disorganized'}
+        onOpenChange={setOpen}
+      >
+        <CollapsibleContent className="data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up -mt-1 mb-1 w-full overflow-hidden motion-reduce:transition-none">
+          <div className="-mt-2 w-full rounded-b-md border-2 border-t-0 border-black bg-red-300 p-2">
             <FieldGroup className="w-full max-w-xs p-2">
               <FieldSet>
-                <FieldGroup className="gap-3">
+                <FieldGroup className="gap-3 pl-3">
                   {issues.map((issue) => (
                     <Field orientation="horizontal" key={issue.id}>
                       <Checkbox id={issue.id} />
@@ -104,11 +149,32 @@ export const CardPlace = ({ place, issues }: PlaceProtocol) => {
               </FieldSet>
             </FieldGroup>
             <div className="flex flex-col items-center">
+              <Field className="p-4 pl-5">
+                <FieldLabel htmlFor="textarea-message" className="mt-2 -mb-2">
+                  Adicionar descrição
+                </FieldLabel>
+                {/* <FieldDescription>Enter your message below.</FieldDescription> */}
+                <Textarea
+                  className="p-3"
+                  id="textarea-message"
+                  placeholder="Adicione aqui a descrição do Check-in."
+                />
+              </Field>
+            </div>
+            <div className="mb-2 flex flex-col items-center pl-5">
+              <Field>
+                <FieldLabel>Adicionar Arquivos</FieldLabel>
+              </Field>
               <FileUploadCircularProgress />
             </div>
           </div>
         </CollapsibleContent>
       </Collapsible>
+
+      {/* Collapsible fora do Item, mas colado */}
+      {/* <Collapsible open={status === 'disorganized'}>
+        
+      </Collapsible> */}
     </>
   )
 }
