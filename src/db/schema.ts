1b1f47a14d5aedf042b9dd96eed00213297b155c
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-09 10:47:05 -0300

    feat: create database for project

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,122 +1,94 @@
+import { date, index, integer, pgEnum, pgTable, primaryKey, text, timestamp, unique, uuid } from "drizzle-orm/pg-core";
 
-// import { varchar } from "drizzle-orm/mysql-core";
-// import {
-//   pgTable,
-//   text,
-//   timestamp,
-//   date,
-//   uuid,
-//   unique, 
-// } from "drizzle-orm/pg-core";
-// import { pgEnum } from "drizzle-orm/pg-core";
+export const checkinStatusEnum = pgEnum("checkin_status", [
+  "organized",
+  "disorganized",
+  "not_checked"
+]);
 
-// export const checkinStatusEnum = pgEnum("checkin_status", [
-//   "organized",
-//   "disorganized",
-// ]);
+export const laboratories = pgTable("laboratories", {
+  id: uuid('id').defaultRandom().primaryKey().notNull().unique(),
+  name: text('name').notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+})
 
-// /* ================================
-//    RESPONSÁVEIS
-// ================================ */
-// export const users = pgTable("users", {
-//   id: uuid('id').defaultRandom().primaryKey(),
-//   name: text("name").notNull(),
-// });
+export const places = pgTable('places', {
+  id: uuid('id').defaultRandom().primaryKey().notNull().unique(),
+  labId: uuid('lab_id').references(() => laboratories.id, { onDelete: 'cascade' }),
+  name: text('name').notNull(),
+  sortOrder: integer('sort_order').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+},
+  (table) => [
+    index('idx_places_lab').on(table.labId)
+  ]
+)
 
-//   /* ================================
-//      SALAS / LABORATORIOS
-//   ================================ */
+export const problems = pgTable('problems', {
+  id: uuid('id').defaultRandom().primaryKey().notNull().unique(),
+  name: text('name').notNull().unique(),
+  description: text('description'),
+})
 
-// export const lab = pgTable("lab", {
-//   id: uuid('id').defaultRandom().primaryKey(),
-//   name: text("name").notNull(),
-// });
-// /* ================================
-//    LOCAIS (BANCADAS / GAVETEIROS)
-// ================================ */
-// export const places = pgTable("places", {
-//   id: uuid('id').defaultRandom().primaryKey(),
-//   name: text("name").notNull(), // Ex: Bancada 1, Gaveteiro A
-//   labId: uuid("lab_id").references(() => lab.id).notNull()
-// });
+export const placeProblems = pgTable('place_problems', {
+  placeId: uuid('place_id').references(() => places.id),
+  problemId: uuid('problem_id').references(() => problems.id)
+},
+  (table) => [
+    primaryKey({ columns: [table.placeId, table.problemId] })
+  ]
+) 
 
-// /* ================================
-//    TIPOS DE PROBLEMAS / CHECKS
-// ================================ */
-// export const issues = pgTable("issues", {
-//   id: uuid('id').defaultRandom().primaryKey(),
-//   code: text("code").notNull(),
-//   description: text("description").notNull(),
-// });
+export const people = pgTable('people', {
+  id: uuid('id').defaultRandom().primaryKey().notNull().unique(),
+  name: text('name').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull()
+})
 
-// /* ================================
-//    CHECK-INS
-// ================================ */
-// export const checkins = pgTable("checkins", {
-//   id: uuid('id').defaultRandom().primaryKey(),
-//   date: date("date").notNull().unique(),
-//   userId: uuid("user_id")
-//     .references(() => users.id)
-//     .notNull(),
-//   createdAt: timestamp("created_at").defaultNow().notNull(),
-// });
+export const checkins = pgTable('checkins', {
+  id: uuid('id').defaultRandom().primaryKey().notNull().unique(),
+  peopleId: uuid('people_id').references(() => people.id).notNull(),
+  date: date('date').notNull(),
+  observation: text('observation'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+},
+  (table) => [
+    index('idx_checkins_date').on(table.date),
+    index('idx_checkins_people').on(table.peopleId),
+  ]
+)
 
-// /* ================================
-//    PROBLEMAS POR LOCAL
-// ================================ */
-// export const checkinPlaceIssues = pgTable("checkin_place_issues", {
-//     id: uuid('id').defaultRandom().primaryKey(),
+export const checkinItems = pgTable('checkin_items', {
+  id: uuid('id').defaultRandom().primaryKey().notNull().unique(),
+  checkinId: uuid('checkin_id').references(() => checkins.id, { onDelete: 'cascade' }).notNull(),
+  placeId: uuid('place_id').references(() => places.id).notNull(),
+  status: checkinStatusEnum("status").notNull(),
+  observation: text('observation'),
+},
+  (table) => [
+    index('idx_checkin_items_checkin').on(table.checkinId),
+    index('idx_checkin_items_place').on(table.placeId),
 
-//     checkinPlaceId: uuid("checkin_places_id")
-//       .references(() => checkinPlaces.id)
-//       .notNull(),
+    unique('unique_checkin_place').on(table.checkinId, table.placeId),
+  ]
+)
 
-//     issueId: uuid("issue_id")
-//       .references(() => issues.id)
-//       .notNull(),
-//   },
-//   (table) => ({
-//     uniqueIssuePerPlace: unique().on(
-//       table.checkinPlaceId,
-//       table.issueId
-//     ),
-//   })
-// );
+export const checkinItemsProblems = pgTable('checkin_items_problems', {
+  id: uuid('id').defaultRandom().primaryKey().notNull().unique(),
+  checkinItemId: uuid('checkin_item_id').references(() => checkinItems.id, { onDelete: "cascade"}),
+  photo_url: text('photo_url'),
+  sortOrder: integer('sort_order'),
+},
+  (table) => [
+    index('idx_checkin_items_problems_item').on(table.checkinItemId)
+  ]
+)
 
-// /* ================================
-//    FOTOS
-// ================================ */
-// export const photos = pgTable("photos", {
-//   id: uuid('id').defaultRandom().primaryKey(),
-//   checkinPlaceId: uuid("checkin_place_id")
-//     .references(() => checkinPlaces.id)
-//     .notNull(),
-//   url: text("url").notNull(),
-// });
-
-// export const checkinPlaces = pgTable("checkin_places", {
-//   id: uuid('id').defaultRandom().primaryKey(),
-//   checkinId: uuid("checkin_id")
-//     .references(() => checkins.id)
-//     .notNull(),
-//   placeId: uuid("place_id")
-//     .references(() => places.id)
-//     .notNull(),
-//   status: checkinStatusEnum("status").notNull(),
-//   observation: text("observation")
-// }, 
-//   (table) => ({
-//     uniqueCheckinPlace: unique().on(table.checkinId, table.placeId)
-//   })
-// )
-
-// export const checkinAuditLogs = pgTable("checkin_audit_logs", {
-//   id: uuid('id').defaultRandom().primaryKey(),
-//   checkinId: uuid("checkin_id")
-//     .references(() => checkins.id)
-//     .notNull(),
-//   reason: text("reason").notNull(),
-//   action: text("action").notNull(),
-//   createdAt: timestamp("created_at").defaultNow().notNull()
-// })
-
+export const checkinEdits = pgTable('checkin_edits', {
+  id: uuid('id').primaryKey().defaultRandom().unique().notNull(),
+  checkinId: uuid('checkin_id').references(() => checkins.id),
+  editedBy: text('edited_by'),
+  reason: text('reason'),
+  changes: text('changes'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+})
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,5 +1,5 @@
 import { dropTables } from './dropTables'
 
 export async function seed() {
-  dropTables()
+
 }
