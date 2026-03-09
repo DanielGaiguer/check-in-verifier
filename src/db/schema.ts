36b9bd9b829ab3c679b1093762b68cc6d561d0d6
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-09 08:44:09 -0300

    reset project

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,121 +1,122 @@
 
-import { varchar } from "drizzle-orm/mysql-core";
-import {
-  pgTable,
-  text,
-  timestamp,
-  date,
-  uuid,
-  unique, 
-} from "drizzle-orm/pg-core";
-import { pgEnum } from "drizzle-orm/pg-core";
+// import { varchar } from "drizzle-orm/mysql-core";
+// import {
+//   pgTable,
+//   text,
+//   timestamp,
+//   date,
+//   uuid,
+//   unique, 
+// } from "drizzle-orm/pg-core";
+// import { pgEnum } from "drizzle-orm/pg-core";
 
-export const checkinStatusEnum = pgEnum("checkin_status", [
-  "organized",
-  "disorganized",
-]);
+// export const checkinStatusEnum = pgEnum("checkin_status", [
+//   "organized",
+//   "disorganized",
+// ]);
 
-/* ================================
-   RESPONSÁVEIS
-================================ */
-export const users = pgTable("users", {
-  id: uuid('id').defaultRandom().primaryKey(),
-  name: text("name").notNull(),
-});
+// /* ================================
+//    RESPONSÁVEIS
+// ================================ */
+// export const users = pgTable("users", {
+//   id: uuid('id').defaultRandom().primaryKey(),
+//   name: text("name").notNull(),
+// });
 
-  /* ================================
-     SALAS / LABORATORIOS
-  ================================ */
+//   /* ================================
+//      SALAS / LABORATORIOS
+//   ================================ */
 
-export const lab = pgTable("lab", {
-  id: uuid('id').defaultRandom().primaryKey(),
-  name: text("name").notNull(),
-});
-/* ================================
-   LOCAIS (BANCADAS / GAVETEIROS)
-================================ */
-export const places = pgTable("places", {
-  id: uuid('id').defaultRandom().primaryKey(),
-  name: text("name").notNull(), // Ex: Bancada 1, Gaveteiro A
-  labId: uuid("lab_id").references(() => lab.id).notNull()
-});
+// export const lab = pgTable("lab", {
+//   id: uuid('id').defaultRandom().primaryKey(),
+//   name: text("name").notNull(),
+// });
+// /* ================================
+//    LOCAIS (BANCADAS / GAVETEIROS)
+// ================================ */
+// export const places = pgTable("places", {
+//   id: uuid('id').defaultRandom().primaryKey(),
+//   name: text("name").notNull(), // Ex: Bancada 1, Gaveteiro A
+//   labId: uuid("lab_id").references(() => lab.id).notNull()
+// });
 
-/* ================================
-   TIPOS DE PROBLEMAS / CHECKS
-================================ */
-export const issues = pgTable("issues", {
-  id: uuid('id').defaultRandom().primaryKey(),
-  code: text("code").notNull(),
-  description: text("description").notNull(),
-});
+// /* ================================
+//    TIPOS DE PROBLEMAS / CHECKS
+// ================================ */
+// export const issues = pgTable("issues", {
+//   id: uuid('id').defaultRandom().primaryKey(),
+//   code: text("code").notNull(),
+//   description: text("description").notNull(),
+// });
 
-/* ================================
-   CHECK-INS
-================================ */
-export const checkins = pgTable("checkins", {
-  id: uuid('id').defaultRandom().primaryKey(),
-  date: date("date").notNull().unique(),
-  userId: uuid("user_id")
-    .references(() => users.id)
-    .notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-});
+// /* ================================
+//    CHECK-INS
+// ================================ */
+// export const checkins = pgTable("checkins", {
+//   id: uuid('id').defaultRandom().primaryKey(),
+//   date: date("date").notNull().unique(),
+//   userId: uuid("user_id")
+//     .references(() => users.id)
+//     .notNull(),
+//   createdAt: timestamp("created_at").defaultNow().notNull(),
+// });
 
-/* ================================
-   PROBLEMAS POR LOCAL
-================================ */
-export const checkinPlaceIssues = pgTable("checkin_place_issues", {
-    id: uuid('id').defaultRandom().primaryKey(),
+// /* ================================
+//    PROBLEMAS POR LOCAL
+// ================================ */
+// export const checkinPlaceIssues = pgTable("checkin_place_issues", {
+//     id: uuid('id').defaultRandom().primaryKey(),
 
-    checkinPlaceId: uuid("checkin_places_id")
-      .references(() => checkinPlaces.id)
-      .notNull(),
+//     checkinPlaceId: uuid("checkin_places_id")
+//       .references(() => checkinPlaces.id)
+//       .notNull(),
 
-    issueId: uuid("issue_id")
-      .references(() => issues.id)
-      .notNull(),
-  },
-  (table) => ({
-    uniqueIssuePerPlace: unique().on(
-      table.checkinPlaceId,
-      table.issueId
-    ),
-  })
-);
+//     issueId: uuid("issue_id")
+//       .references(() => issues.id)
+//       .notNull(),
+//   },
+//   (table) => ({
+//     uniqueIssuePerPlace: unique().on(
+//       table.checkinPlaceId,
+//       table.issueId
+//     ),
+//   })
+// );
 
-/* ================================
-   FOTOS
-================================ */
-export const photos = pgTable("photos", {
-  id: uuid('id').defaultRandom().primaryKey(),
-  checkinPlaceId: uuid("checkin_place_id")
-    .references(() => checkinPlaces.id)
-    .notNull(),
-  url: text("url").notNull(),
-});
+// /* ================================
+//    FOTOS
+// ================================ */
+// export const photos = pgTable("photos", {
+//   id: uuid('id').defaultRandom().primaryKey(),
+//   checkinPlaceId: uuid("checkin_place_id")
+//     .references(() => checkinPlaces.id)
+//     .notNull(),
+//   url: text("url").notNull(),
+// });
 
-export const checkinPlaces = pgTable("checkin_places", {
-  id: uuid('id').defaultRandom().primaryKey(),
-  checkinId: uuid("checkin_id")
-    .references(() => checkins.id)
-    .notNull(),
-  placeId: uuid("place_id")
-    .references(() => places.id)
-    .notNull(),
-  status: checkinStatusEnum("status").notNull(),
-  observation: text("observation")
-}, 
-  (table) => ({
-    uniqueCheckinPlace: unique().on(table.checkinId, table.placeId)
-  })
-)
+// export const checkinPlaces = pgTable("checkin_places", {
+//   id: uuid('id').defaultRandom().primaryKey(),
+//   checkinId: uuid("checkin_id")
+//     .references(() => checkins.id)
+//     .notNull(),
+//   placeId: uuid("place_id")
+//     .references(() => places.id)
+//     .notNull(),
+//   status: checkinStatusEnum("status").notNull(),
+//   observation: text("observation")
+// }, 
+//   (table) => ({
+//     uniqueCheckinPlace: unique().on(table.checkinId, table.placeId)
+//   })
+// )
 
-export const checkinAuditLogs = pgTable("checkin_audit_logs", {
-  id: uuid('id').defaultRandom().primaryKey(),
-  checkinId: uuid("checkin_id")
-    .references(() => checkins.id)
-    .notNull(),
-  reason: text("reason").notNull(),
-  action: text("action").notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull()
-})
+// export const checkinAuditLogs = pgTable("checkin_audit_logs", {
+//   id: uuid('id').defaultRandom().primaryKey(),
+//   checkinId: uuid("checkin_id")
+//     .references(() => checkins.id)
+//     .notNull(),
+//   reason: text("reason").notNull(),
+//   action: text("action").notNull(),
+//   createdAt: timestamp("created_at").defaultNow().notNull()
+// })
+
--- src/services/users.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "@/types/checkin"
-// import { Places } from "@/types/places"
-
-export async function getUsers(): Promise<User[]> {
-	const response = await fetch(`${process.env.URL}/api/users`, {cache: 'no-store'})
-
-	return response.json()
-}
