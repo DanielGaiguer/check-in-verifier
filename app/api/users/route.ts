9be1044c7073a8895c464c27f9f826d8c1c594a8
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-09 14:04:45 -0300

    delete routes

--- app/api/users/route.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/db";
-import { users } from "@/db/schema";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-	const data = await db.select().from(users)
-
-	return NextResponse.json(data)
-}
