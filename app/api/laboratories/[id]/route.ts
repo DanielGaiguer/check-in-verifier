a64b2378d41ca7ffba36f4065df7f59ef14ae9e0
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-10 10:00:44 -0300

    cfeat: create method patch for labs in API

--- app/api/laboratories/[id]/route.ts
+++ app/api/laboratories/[id]/route.ts
@@ -1,5 +1,31 @@
-export async function PATCH(req: Request, context: { params: Promise<{id: string}> }) {
-	const id = await context.params
+import { db } from "@/db"
+import { laboratories } from "@/db/schema"
+import { eq } from "drizzle-orm"
+import { NextResponse } from "next/server"
 
-	
+export async function PATCH(req: Request, context: { params: Promise<{id: string}> }) {
+	const { id } = await context.params
+	const body = await req.json()
+
+	if (!id) {
+		return NextResponse.json({
+			success: false,
+			error: "ID do laboratório não informado."
+		}, {status: 400})
+	}
+
+	try{
+		await db.update(laboratories).set({name: body.name}).where(eq(laboratories.id, id)).returning()
+	}catch(e) {
+		NextResponse.json({
+			success: false,
+			error: e
+		}, { status: 400})
+	}
+
+	return NextResponse.json({
+		success: true,
+		data: `Laboratório ${id} alterado com sucesso.`
+	})
+
 }
