fc8f557b5ea82616a9dbedad19854f966c65ebc1
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-10 10:26:23 -0300

    feat: create method GET for people in API

--- app/api/people/route.ts
+++ app/api/people/route.ts
@@ -1,3 +1,21 @@
+import { db } from "@/db";
+import { people } from "@/db/schema";
+import { NextResponse } from "next/server";
+
 export async function GET(req: Request) {
-	
+	let response
+	try{
+		response = await db.select().from(people)
+	}catch(e) {
+		return NextResponse.json({
+			success: false,
+			error: e
+		}, {status: 400})
+	}
+
+	return NextResponse.json({
+		success: true,
+		data: response,
+		count: response.length
+	})
 }
