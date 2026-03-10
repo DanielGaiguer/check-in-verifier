2676c1a8eeca6f8e6d28775afa0b6162388e04bc
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-10 09:19:31 -0300

    feat: create GET from API

--- app/api/checkins/route.ts
+++ app/api/checkins/route.ts
@@ -1,19 +1,31 @@
-import { SearchParamsProps, switchWheteClause } from "@/services/switchWhereClause"
+import { db } from '@/db'
+import { checkins } from '@/db/schema'
+import {
+  SearchParamsProps,
+  switchWheteClause,
+} from '@/services/switchWhereClause'
+import { between, eq } from 'drizzle-orm'
 
-export async function GET(req: Request){
-	const { searchParams } = new URL(req.url)
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url)
 
-	const params: SearchParamsProps = {
-		range: searchParams.get('range') as SearchParamsProps['range'] | undefined,
-		from: searchParams.get('from') || undefined,
-		to: searchParams.get('to') || undefined,
-	}
+  const params: SearchParamsProps = {
+    range: searchParams.get('range') as SearchParamsProps['range'] | undefined,
+    from: searchParams.get('from') || undefined,
+    to: searchParams.get('to') || undefined,
+  }
 
-	const whereClause = switchWheteClause(params)
+  const whereClause = switchWheteClause(params)
 
-	return new Response(JSON.stringify({ whereClause }), {
-		status: 200,
-		headers: { 'Contenty-Type': 'application/json'}
-	})
-	
+  const checkinsFilter = await db
+      .select({
+				checkinsDate: checkins.date
+			})
+      .from(checkins)
+      .where(whereClause)
+
+  return new Response(JSON.stringify({ checkinsFilter }), {
+    status: 200,
+    headers: { 'Contenty-Type': 'application/json' },
+  })
 }
--- src/services/switchWhereClause.ts
+++ src/services/switchWhereClause.ts
@@ -1,19 +1,50 @@
+import { checkins } from "@/db/schema"
+import { format, subMonths, subWeeks, subYears } from "date-fns"
+import { between, eq, SQL } from "drizzle-orm"
+
 export interface SearchParamsProps {
   range?: 'today' | 'week' | 'month'
   from?: string
   to?: string
 }
 
-export function switchWheteClause(searchParams: SearchParamsProps){
+
+export function switchWheteClause(searchParams: SearchParamsProps): SQL | undefined{
+	const date = new Date()
+	const today = format(date, "yyyy-MM-dd")
+
 	if (searchParams.range === 'today') {
-		return ('hoje')
+		return eq(checkins.date, today)
 	}
 	
 	if (searchParams.range === 'week') {
-		return ('semana')
+		const week = format(subWeeks(date, 1), "yyyy-MM-dd")
+		return between(checkins.date, week, today)
 	}
 
-	if (searchParams.range === 'month') {
-		return ('mes')
-	}
+  if (searchParams.range === 'month') {
+    const month = format(subMonths(date, 1), "yyyy-MM-dd")
+    return between(checkins.date, month, today)
+  }
+
+  if (searchParams.range === '3-month') {
+    const month = format(subMonths(date, 3), "yyyy-MM-dd")
+    return between(checkins.date, month, today)
+  }
+
+  if (searchParams.range === '6-month') {
+    const month = format(subMonths(date, 6), "yyyy-MM-dd")
+    return between(checkins.date, month, today)
+  }
+
+  if (searchParams.range === 'year') {
+    const year = format(subYears(date, 1), "yyyy-MM-dd")
+    return between(checkins.date, year, today)
+  }
+
+  if (searchParams.from && searchParams.to) {
+    return between(checkins.date, searchParams.from, searchParams.to)
+  }
+
+  return undefined
 }
