2aa5c05189674080cb53de44a98c19f1e9c5f7d1
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-10 11:51:40 -0300

    feat: create method POST for place in API

--- app/api/places/route.ts
+++ app/api/places/route.ts
@@ -1,21 +1,96 @@
-import { db } from "@/db"
-import { places } from "@/db/schema"
-import { NextResponse } from "next/server"
+import { db } from '@/db'
+import { places } from '@/db/schema'
+import { and, eq, gte, max, sql } from 'drizzle-orm'
+import { NextResponse } from 'next/server'
+
+interface PostPlaceProtocol {
+  labId: string
+  name: string
+  sortOrder?: number
+}
 
 export async function GET(req: Request) {
-	let response
+  let response
 
-	try{
-		response = await db.select().from(places)
-	}catch(e){
-		NextResponse.json({
-			success: false,
-			error: e
-		}, {status: 400})
-	}
+  try {
+    response = await db.select().from(places)
+  } catch (e) {
+    NextResponse.json(
+      {
+        success: false,
+        error: e,
+      },
+      { status: 400 }
+    )
+  }
+
+  return NextResponse.json({
+    success: true,
+    data: response,
+  })
+}
+
+export async function POST(req: Request,) {
+  const body: PostPlaceProtocol = await req.json()
+	let inserted
+
+  if (!body.labId || !body.name) {
+    return NextResponse.json({
+      success: false,
+      error: `Informações sobre o lugar estão faltando.`,
+    })
+  }
+
+  if (!body.sortOrder) {
+    try {
+      const result = await db
+        .select({ maxSort: max(places.sortOrder) })
+        .from(places)
+        .where(eq(places.labId, body.labId))
+      let maxSortOrder = result[0]?.maxSort
+      body.sortOrder = (maxSortOrder || 0) + 1
+    } catch (e) {
+      return NextResponse.json({
+        success: false,
+        error: e,
+      }, {status: 400})
+    }
+  }
+
+  if (body.sortOrder) {
+		try{
+			await db
+				.update(places)
+				.set({
+					sortOrder: sql`${places.sortOrder} + 1`,
+				})
+				.where(
+					and(eq(places.labId, body.labId), gte(places.sortOrder, body.sortOrder))
+				)
+	
+			inserted = await db.insert(places).values({
+				labId: body.labId,
+				name: body.name,
+				sortOrder: body.sortOrder,
+			}).returning()
+
+		}catch(e) {
+			return NextResponse.json({
+				success: false,
+				error: e
+			}, { status: 400})
+		}
+  }
+	
+	if (!inserted || !inserted[0]) {
+  return NextResponse.json({
+    success: false,
+    error: "Não foi possível inserir o lugar.",
+  }, { status: 500 })
+}
 
 	return NextResponse.json({
 		success: true,
-		data: response
+		data: inserted[0]
 	})
-} 
+}
