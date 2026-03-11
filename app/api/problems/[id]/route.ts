bfac3327ce587b4b5c48d6d78b1865c47f74ff01
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-11 09:04:21 -0300

    feat: create method PATCH for prtoblems in API

---
+++ app/api/problems/[id]/route.ts
@@ -0,0 +1,29 @@
+import { db } from "@/db"
+import { problems } from "@/db/schema"
+import { eq } from "drizzle-orm"
+import { NextResponse } from "next/server"
+
+export async function PATCH(req: Request, context:{ params: Promise<{id: string}>}) {
+	const { id } = await context.params
+	const body = await req.json()
+
+	if (!body.name || !body.description) {
+		NextResponse.json({
+			success: false,
+			error: "Dados para atualização estão faltando."
+		})
+	}
+	try{
+		await db.update(problems).set({name: body.name, description: body.description}).where(eq(problems.id, id))
+	}catch(e) {
+		NextResponse.json({
+			success: false,
+			error: e
+		}, {status: 400})
+	}
+
+	return NextResponse.json({
+		success: true,
+		data: `Problema ${body.name} atualizado com sucesso`
+	})
+}
