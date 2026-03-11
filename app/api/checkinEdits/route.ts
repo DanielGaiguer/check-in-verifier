d4f34087a7b4eaccd42c16850c47db6aeaf98b7e
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-11 11:50:48 -0300

    feat: create method POST for edit check-in

--- app/api/checkinEdits/route.ts
+++ app/api/checkinEdits/route.ts
@@ -38,3 +38,37 @@ export async function GET(req: Request) {
 	})
 }
 
+export async function POST(req: Request) {
+	const body = await req.json()
+
+	if (!body.checkinId || !body.problemId || !body.editedBy || !body.reason) {
+		return NextResponse.json(
+			{
+				success: false,
+				error: 'Dados incompletos',
+			},
+			{ status: 400 }
+		)
+	}
+	try {
+		await db.insert(checkinEdits).values({
+			checkinId: body.checkinId,
+			editedBy: body.editedBy,
+			reason: body.reason,
+			changes: body.changes ?? ""
+		})
+	} catch (e) {
+		return NextResponse.json(
+			{
+				success: false,
+				error: e,
+			},
+			{ status: 400 }
+		)
+	}
+
+	return NextResponse.json({
+		success: true,
+		data: 'Alteracao do check-in criado com sucesso.',
+	})
+}
