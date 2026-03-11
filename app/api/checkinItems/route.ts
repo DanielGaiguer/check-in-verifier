cef55d4b9ffe7f38e2d606929e67b168f51cf48f
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-11 10:31:49 -0300

    feat: create method POST for item checkin

--- app/api/checkinItems/route.ts
+++ app/api/checkinItems/route.ts
@@ -1,20 +1,58 @@
-import { db } from "@/db"
-import { checkinItems } from "@/db/schema"
-import { NextResponse } from "next/server"
+import { db } from '@/db'
+import { checkinItems } from '@/db/schema'
+import { NextResponse } from 'next/server'
 
 export async function GET() {
-	let result
-		try {
-			result = await db.select().from(checkinItems)
-		} catch (e) {
-			return NextResponse.json({
-				success: false, 
-				error: e
-			}, { status: 400})
-		}
-	
-		return NextResponse.json({
-			success: true,
-			data: result
-		})
+  let result
+  try {
+    result = await db.select().from(checkinItems)
+  } catch (e) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: e,
+      },
+      { status: 400 }
+    )
+  }
+
+  return NextResponse.json({
+    success: true,
+    data: result,
+  })
+}
+
+export async function POST(req: Request) {
+  const body = await req.json()
+
+  if (!body.checkinId || !body.placeId || !body.status) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: 'Dados incompletos',
+      },
+      { status: 400 }
+    )
+  }
+  try {
+    await db.insert(checkinItems).values({
+      checkinId: body.checkinId,
+      placeId: body.placeId,
+      status: body.status,
+      observation: body.observation ?? "",
+    })
+  } catch (e) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: e,
+      },
+      { status: 400 }
+    )
+  }
+
+  return NextResponse.json({
+    success: true,
+    data: 'Dados do check-in alterados com sucesso.',
+  })
 }
