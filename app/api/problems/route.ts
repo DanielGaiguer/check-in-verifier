dc9dadefe3e87e7790c64bb00bb76bd627ac6413
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-11 08:54:33 -0300

    feat: create method POST for problems in API

--- app/api/problems/route.ts
+++ app/api/problems/route.ts
@@ -3,7 +3,6 @@ import { problems } from "@/db/schema"
 import { NextResponse } from "next/server"
 
 export async function GET(req: Request) {
-
 	try{
 		const result =  await db.select().from(problems)
 
@@ -18,4 +17,24 @@ export async function GET(req: Request) {
 			error: e
 		}, {status: 400})
 	}
+}
+
+export async function POST(req: Request) {
+	const body = await req.json()
+
+	let result
+
+	try{
+		result = await db.insert(problems).values({name: body.name, description: body.description}).returning()
+	}catch(e) {
+		return NextResponse.json({
+			success: false,
+			error: e
+		}, { status: 400})		
+	}
+
+	return NextResponse.json({
+		success: true,
+		data: `Dados inseridos: ${result}`
+	})
 }
