67958bf8183fdfa453789a714ae561dd458a31f0
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-12 15:15:59 -0300

    feat: change color

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 const Header = () => {
-  return <div className="md:sticky top-0 z-40 h-5 md:h-12 flex items-center px-4 border-b border-gray-300 shadow-2xs rounded-t-xl"></div>
+  return <div className="md:sticky top-0 z-40 h-5 md:h-12 flex items-center px-4 border-b border-gray-300 shadow-2xs rounded-t-xl bg-gray-50"></div>
 }
 
 export default Header
