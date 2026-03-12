1a2b0c276a9dac192068cde2f9c03b09f5180a62
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-12 08:13:30 -0300

    feat: Add sidebar

--- src/components/sidebar-sheet.tsx
+++ src/components/sidebar-sheet.tsx
@@ -6,7 +6,7 @@ import {
   ChartNoAxesCombined,
   FlaskConical,
   HistoryIcon,
-  HomeIcon,
+  LayoutDashboardIcon,
 } from 'lucide-react'
 import {
   SheetClose,
@@ -16,82 +16,102 @@ import {
   SheetTitle,
 } from './ui/sheet'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const SidebarSheet = () => {
+  const pathname = usePathname()
+
+  const isActive = pathname === '/'  // ajustar para cada rota
+
   return (
     <SheetContent
       className="w-65 overflow-y-auto bg-[#14181F] [&>button]:scale-125 [&>button]:border-0 [&>button]:bg-transparent [&>button]:p-2 [&>button]:ring-0 [&>button]:outline-none [&>button]:focus:ring-0 [&>button]:focus:ring-offset-0 [&>button]:focus:outline-none [&>button]:focus-visible:ring-0 [&>button]:focus-visible:ring-offset-0 [&>button]:focus-visible:outline-none"
       side="left"
     >
-
-      <SheetHeader className="mt-2 flex flex-row pr-12 border-b border-b-gray-700">
+      <SheetHeader className="mt-2 flex flex-row border-b border-b-gray-700 pr-12">
         <FlaskConical
           color="white"
           className="rounded-xl bg-blue-400 p-2"
           size={35}
         />
         <div>
-          <SheetTitle className="ml-1 text-left text-sm font-bold text-white font-sans">
-            Labcheck
+          <SheetTitle className="ml-1 text-left font-sans text-sm font-bold text-white">
+            LabCheck
           </SheetTitle>
-          <SheetDescription className=" ml-1 text-xs">
+          <SheetDescription className="ml-1 text-xs">
             Check-in de Laboratório
           </SheetDescription>
         </div>
       </SheetHeader>
 
-      <div className="flex flex-col gap-1 border-b border-solid py-5">
+      <div className="flex flex-col gap-1 py-5 ml-5 mr-5 font-sans">
         <SheetClose asChild>
           <Button
-            className="mb-2 justify-start gap-4 text-xl text-gray-600 [&_svg]:size-6!"
-            variant="ghost"
+            className={`mb-2 justify-start gap-4 text-sm ${
+              isActive
+                ? 'bg-gray-800 text-blue-400' // estilo quando selecionado
+                : 'text-gray-600 hover:bg-gray-700 hover:text-white' // estilo normal
+            }`}
+            variant="default"
             asChild
           >
             <Link href="/">
-              <HomeIcon size={18} />
-              Início
+              <LayoutDashboardIcon size={15} />
+              Dashboard
             </Link>
           </Button>
         </SheetClose>
 
         <SheetClose asChild>
-          <Button
-            className="mb-2 justify-start gap-4 text-xl text-gray-600 [&_svg]:size-6!"
-            variant="ghost"
-            asChild
-          >
-            <Link href="/checkins/create">
-              <CalendarIcon size={18} />
-              Realizar Check-in
-            </Link>
-          </Button>
-        </SheetClose>
+        <Button
+          className={`mb-2 justify-start gap-4 text-sm ${
+            pathname === '/checkins/create'
+              ? 'bg-gray-800 text-white'
+              : 'text-gray-400 hover:bg-gray-700 hover:text-white'
+          }`}
+          variant="default"
+          asChild
+        >
+          <Link href="/checkins/create">
+            <CalendarIcon size={15} />
+            Realizar Check-in
+          </Link>
+        </Button>
+      </SheetClose>
 
-        <SheetClose asChild>
-          <Button
-            className="mb-2 justify-start gap-4 text-xl text-gray-600 [&_svg]:size-6!"
-            variant="ghost"
-            asChild
-          >
-            <Link href="/bookings">
-              <HistoryIcon size={18} />
-              Histórico de Check-ins
-            </Link>
-          </Button>
-        </SheetClose>
+      <SheetClose asChild>
+        <Button
+          className={`mb-2 justify-start gap-4 text-sm ${
+            pathname === '/bookings'
+              ? 'bg-gray-800 text-white'
+              : 'text-gray-400 hover:bg-gray-700 hover:text-white'
+          }`}
+          variant="default"
+          asChild
+        >
+          <Link href="/bookings">
+            <HistoryIcon size={15} />
+            Histórico de Check-ins
+          </Link>
+        </Button>
+      </SheetClose>
 
-        <SheetClose asChild>
-          <Button
-            className="mb-2 justify-start gap-4 text-xl text-gray-600 [&_svg]:size-6!"
-            variant="ghost"
-            asChild
-          >
-            <Link href="/bookings">
-              <ChartNoAxesCombined size={18} />
-              Relatórios de Check-ins
-            </Link>
-          </Button>
-        </SheetClose>
+      <SheetClose asChild>
+        <Button
+          className={`mb-2 justify-start gap-4 text-sm ${
+            pathname === '/bookings'
+              ? 'bg-gray-800 text-white'
+              : 'text-gray-400 hover:bg-gray-700 hover:text-white'
+          }`}
+          variant="default"
+          asChild
+        >
+          <Link href="/bookings">
+            <ChartNoAxesCombined size={15} />
+            Relatórios de Check-ins
+          </Link>
+        </Button>
+      </SheetClose>
       </div>
 
       {/* <div className="flex flex-col gap-1 border-b border-solid py-5">
