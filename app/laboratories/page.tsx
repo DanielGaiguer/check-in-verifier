03d4e2232239ed8ce454b1f52a39ab870504a103
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-18 09:49:01 -0300

    feat: create dialog for labs

--- app/laboratories/page.tsx
+++ app/laboratories/page.tsx
@@ -2,16 +2,30 @@
 import { EditCard } from '@/components/edit-card'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import { useLaboratories } from '@/hooks/useQuerys/useLaboratories'
 import { FlaskConicalIcon, PlusIcon } from 'lucide-react'
-
+import { useState } from 'react'
 
 export default function LaboratoriesPage() {
-  const { laboratories, isLoading, error} = useLaboratories()
+  const { laboratories, isLoading, error } = useLaboratories()
+  const [name, setName] = useState("")
+  const [dialogOpen, setDialogOpen] = useState(false)
 
   if (isLoading) return <p>Carregando...</p>
   if (error) return <p>Erro ao buscar laboratórios</p>
 
+  function handleSubmit() {
+    console.log('enviou')
+  }
   return (
     <main className="mx-auto flex min-h-screen w-full max-w-6xl flex-col justify-start rounded-t-xl bg-gray-50 md:mt-2">
       <div className="m-5 flex-1 rounded-t-xl bg-gray-50">
@@ -25,10 +39,51 @@ export default function LaboratoriesPage() {
             </h4>
           </div>
           <div>
-            <Button className="w-40 rounded-md bg-blue-400 p-5 font-sans text-white hover:bg-blue-300">
-              <PlusIcon className="mr-1 mb-0.5" />
-              Novo Laboratório
-            </Button>
+            <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+              <DialogTrigger asChild>
+                <Button className="w-40 rounded-md bg-blue-400 p-5 font-sans text-white hover:bg-blue-300">
+                  <PlusIcon className="mr-1 mb-0.5" />
+                  Novo Laboratório
+                </Button>
+              </DialogTrigger>
+
+              <DialogContent className="max-h-[85vh] w-full max-w-md overflow-y-auto p-6">
+                <DialogHeader>
+                  <DialogTitle className="text-lg font-semibold">
+                    Novo Laboratório
+                  </DialogTitle>
+                </DialogHeader>
+
+                <form onSubmit={handleSubmit} className="space-y-4">
+                  <div>
+                    <Input
+                      id="name"
+                      placeholder="Nome do laboratório"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      className="focus:border-2.5 flex h-10 w-full items-center justify-between rounded-md border-2 border-gray-300 bg-gray-100! px-3 focus:border-blue-400!"
+                    />
+                  </div>
+                  <div className="mt-4 flex justify-end gap-2">
+                    <Button
+                      type="button"
+                      variant="outline"
+                      onClick={() => setDialogOpen(false)}
+                      className="cursor-pointer"
+                    >
+                      Cancelar
+                    </Button>
+                    <Button
+                      type="submit"
+                      disabled={!name.trim() || !name}
+                      className="cursor-pointer bg-blue-400 hover:bg-blue-300"
+                    >
+                      Criar
+                    </Button>
+                  </div>
+                </form>
+              </DialogContent>
+            </Dialog>
           </div>
         </div>
         <div className="mt-5">
