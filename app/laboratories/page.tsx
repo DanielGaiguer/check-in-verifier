8da90c794b3e43fbe6595f03c7f246527293af6d
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-19 08:34:08 -0300

    feat: create dialog laboratories component and apply

--- app/laboratories/page.tsx
+++ app/laboratories/page.tsx
@@ -1,24 +1,14 @@
 'use client'
+import DialogLaboratories from '@/components/dialog-laboratories'
 import { EditCard } from '@/components/edit-card'
-import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { useLaboratories } from '@/hooks/useQuerys/useLaboratories'
-import { FlaskConicalIcon, PlusIcon } from 'lucide-react'
+import { FlaskConicalIcon } from 'lucide-react'
 import { useState } from 'react'
 
 export default function LaboratoriesPage() {
   const { laboratories, isLoading, error } = useLaboratories()
   const [name, setName] = useState("")
-  const [dialogOpen, setDialogOpen] = useState(false)
 
   if (isLoading) return <p>Carregando...</p>
   if (error) return <p>Erro ao buscar laboratórios</p>
@@ -39,51 +29,7 @@ export default function LaboratoriesPage() {
             </h4>
           </div>
           <div>
-            <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-              <DialogTrigger asChild>
-                <Button className="w-40 rounded-md bg-blue-400 p-5 font-sans text-white hover:bg-blue-300">
-                  <PlusIcon className="mr-1 mb-0.5" />
-                  Novo Laboratório
-                </Button>
-              </DialogTrigger>
-
-              <DialogContent className="max-h-[85vh] w-full max-w-md overflow-y-auto p-6">
-                <DialogHeader>
-                  <DialogTitle className="text-lg font-semibold">
-                    Novo Laboratório
-                  </DialogTitle>
-                </DialogHeader>
-
-                <form onSubmit={handleSubmit} className="space-y-4">
-                  <div>
-                    <Input
-                      id="name"
-                      placeholder="Nome do laboratório"
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                      className="focus:border-2.5 flex h-10 w-full items-center justify-between rounded-md border-2 border-gray-300 bg-gray-100! px-3 focus:border-blue-400!"
-                    />
-                  </div>
-                  <div className="mt-4 flex justify-end gap-2">
-                    <Button
-                      type="button"
-                      variant="outline"
-                      onClick={() => setDialogOpen(false)}
-                      className="cursor-pointer"
-                    >
-                      Cancelar
-                    </Button>
-                    <Button
-                      type="submit"
-                      disabled={!name.trim() || !name}
-                      className="cursor-pointer bg-blue-400 hover:bg-blue-300"
-                    >
-                      Criar
-                    </Button>
-                  </div>
-                </form>
-              </DialogContent>
-            </Dialog>
+            <DialogLaboratories setName={setName} name={name}/>
           </div>
         </div>
         <div className="mt-5">
--- src/components/dialog-people.tsx
+++ src/components/dialog-people.tsx
@@ -6,22 +6,20 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Textarea } from '@/components/ui/textarea'
 import { useState } from 'react'
 import { Button } from './ui/button'
 import { PlusIcon } from 'lucide-react'
 
-interface DialogPeopleProtocol{
-	setName: (arg0: string) => void
-	name: string
+interface DialogPeopleProtocol {
+  setName: (arg0: string) => void
+  name: string
 }
 
-export default function DialogPeople( {setName, name }: DialogPeopleProtocol) {
+export default function DialogPeople({ setName, name }: DialogPeopleProtocol) {
   const [dialogOpen, setDialogOpen] = useState(false)
 
   function handleSubmit(e: { preventDefault: () => void }) {
-		e.preventDefault()
+    e.preventDefault()
     console.log('enviou')
   }
 
@@ -30,14 +28,14 @@ export default function DialogPeople( {setName, name }: DialogPeopleProtocol) {
       <DialogTrigger asChild>
         <Button className="w-40 rounded-md bg-blue-400 p-5 font-sans text-white hover:bg-blue-300">
           <PlusIcon className="mr-1 mb-0.5" />
-          Nova Pessoa
+          Novo Laboratório
         </Button>
       </DialogTrigger>
 
       <DialogContent className="max-h-[85vh] w-full max-w-md overflow-y-auto p-6">
         <DialogHeader>
           <DialogTitle className="text-lg font-semibold">
-            Nova Pessoa
+            Novo Laboratório
           </DialogTitle>
         </DialogHeader>
 
@@ -45,7 +43,7 @@ export default function DialogPeople( {setName, name }: DialogPeopleProtocol) {
           <div>
             <Input
               id="name"
-              placeholder="Nome completo"
+              placeholder="Nome do laboratório"
               value={name}
               onChange={(e) => setName(e.target.value)}
               className="focus:border-2.5 flex h-10 w-full items-center justify-between rounded-md border-2 border-gray-300 bg-gray-100! px-3 focus:border-blue-400!"
