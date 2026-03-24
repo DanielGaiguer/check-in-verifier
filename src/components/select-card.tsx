3d9d7cfddeed31a4ffd65f5810bcfe193fe2f177
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-24 08:59:52 -0300

    feat: add state for select-card

--- src/components/select-card.tsx
+++ src/components/select-card.tsx
@@ -1,34 +1,51 @@
 'use client'
-import { Card, CardHeader } from "./ui/card";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "./ui/select";
+import { usePeople } from '@/hooks/useQuerys/usePeoples'
+import { Card, CardHeader } from './ui/card'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select'
 
-interface selectCardProps{
+interface selectCardProps {
   textHeader: string
   placeHolder: string
   options?: string[]
 }
 
-//TODO: Deixar interativo e retirar o hardCode, adicionar estado
+export default function SelectCard({
+  textHeader,
+  placeHolder,
+  options,
+}: selectCardProps) {
+  const { people, isLoading, error } = usePeople()
 
-export default function SelectCard({textHeader, placeHolder, options}: selectCardProps) {
-	return (
-		<Card className="mt-5">
-			<CardHeader>
-				<h6 className="font-semibold text-sm">{textHeader}</h6>
-    <Select>
-      <SelectTrigger className="w-full max-w-80">
-        <SelectValue placeholder={placeHolder} className="text-sm"/>
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          {/* <SelectLabel>Fruits</SelectLabel> */}
-          <SelectItem value="joao">João</SelectItem>
-          <SelectItem value="daniel">Daniel</SelectItem>
-          <SelectItem value="isis">Isis</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-			</CardHeader>
-		</Card>
-	)
+  
+  if (isLoading) return <p>Carregando...</p>
+  if (error) return <p>Erro ao carregar os pessoas.</p>
+  
+  return (
+    <Card className="mt-5">
+      <CardHeader>
+        <h6 className="text-sm font-semibold">{textHeader}</h6>
+        <Select>
+          <SelectTrigger className="w-full max-w-80">
+            <SelectValue placeholder={placeHolder} className="text-sm" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {/* <SelectLabel>Fruits</SelectLabel> */}
+              {people.map((people) => (
+                <SelectItem value={people.name} key={people.id}>{people.name}</SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </CardHeader>
+    </Card>
+  )
 }
