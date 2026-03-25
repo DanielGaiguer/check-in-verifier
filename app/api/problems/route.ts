301a1cc8394a7e528643b6d5893f75624ed69a43
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-25 10:43:30 -0300

    feat: create soft delete for get route problems

--- app/api/problems/route.ts
+++ app/api/problems/route.ts
@@ -1,40 +1,56 @@
 import { db } from "@/db"
 import { problems } from "@/db/schema"
 import { NextResponse } from "next/server"
+import { eq } from "drizzle-orm"
 
+//GET /api/problems?active=true 
+//GET /api/problems
 export async function GET(req: Request) {
-	try{
-		const result =  await db.select().from(problems)
+  try {
+    const url = new URL(req.url)
+    const activeOnly = url.searchParams.get("active") === "true"
 
-		return NextResponse.json({
-			success: true,
-			data: result,
-			count: result.length
-		})
-	}catch(e) {
-		return NextResponse.json({
-			success: false,
-			error: e
-		}, {status: 400})
-	}
+    const result = await db
+      .select()
+      .from(problems)
+      .where(activeOnly ? eq(problems.active, true) : undefined) // filtra ativos se pedido
+
+    return NextResponse.json({
+      success: true,
+      data: result,
+      count: result.length,
+    })
+  } catch (e) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: e,
+      },
+      { status: 400 }
+    )
+  }
 }
 
 export async function POST(req: Request) {
-	const body = await req.json()
+  const body = await req.json()
 
-	let result
+  try {
+    const result = await db
+      .insert(problems)
+      .values({ name: body.name })
+      .returning()
 
-	try{
-		result = await db.insert(problems).values({name: body.name}).returning()
-	}catch(e) {
-		return NextResponse.json({
-			success: false,
-			error: e
-		}, { status: 400})		
-	}
-
-	return NextResponse.json({
-		success: true,
-		data: result[0]
-	})
+    return NextResponse.json({
+      success: true,
+      data: result[0],
+    })
+  } catch (e) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: e,
+      },
+      { status: 400 }
+    )
+  }
 }
