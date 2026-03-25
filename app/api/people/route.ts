4ac857e335e4acab62101ca722dbf1d6b691b771
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-25 10:49:17 -0300

    feat: add soft delete for people route

--- app/api/people/route.ts
+++ app/api/people/route.ts
@@ -1,46 +1,67 @@
 import { db } from "@/db";
 import { people } from "@/db/schema";
+import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-	let response
-	try{
-		response = await db.select().from(people)
-	}catch(e) {
-		return NextResponse.json({
-			success: false,
-			error: e
-		}, {status: 400})
-	}
+  const url = new URL(req.url);
+  const activeParam = url.searchParams.get("active");
+  // true ou null → apenas ativos, false → todos
+  const onlyActive = activeParam !== "false";
 
-	return NextResponse.json({
-		success: true,
-		data: response,
-		count: response.length
-	})
+  let response;
+  try {
+    response = await db
+      .select()
+      .from(people)
+      .where(onlyActive ? eq(people.active, true) : undefined);
+  } catch (e) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: e,
+      },
+      { status: 400 }
+    );
+  }
+
+  return NextResponse.json({
+    success: true,
+    data: response,
+    count: response.length,
+  });
 }
 
 export async function POST(req: Request) {
-	const body = await req.json()
-	
-	if (!body.name) {
-		return NextResponse.json({
-			success: false,
-			error: "Nome da pessoa não informado"
-		}, {status: 400})
-	}
+  const body = await req.json();
 
-	try{
-		await db.insert(people).values({name: body.name})
-	}catch(e) {
-		NextResponse.json({
-			success: false,
-			error: e
-		}, {status: 400})
-	}
+  if (!body.name) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: "Nome da pessoa não informado",
+      },
+      { status: 400 }
+    );
+  }
 
-	return NextResponse.json({
-		success: true,
-		data: `Pessoa ${body.name} criada com sucesso`
-	})
+  try {
+    await db.insert(people).values({
+      name: body.name,
+      active: true, // padrão ativo
+    });
+  } catch (e) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: e,
+      },
+      { status: 400 }
+    );
+  }
+
+  return NextResponse.json({
+    success: true,
+    data: `Pessoa ${body.name} criada com sucesso`,
+  });
 }
