6f4830e2933236bb4b6f0964c8793535d3dbf8ff
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-03-27 08:25:47 -0300

    feat: add validation for place-problems

--- app/api/place-problems/route.ts
+++ app/api/place-problems/route.ts
@@ -1,44 +1,60 @@
-import { db } from "@/db";
-import { placeProblems } from "@/db/schema";
-import { and, eq } from "drizzle-orm";
-import { NextResponse } from "next/server";
+import { db } from '@/db'
+import { placeProblems } from '@/db/schema'
+import { and, eq } from 'drizzle-orm'
+import { NextResponse } from 'next/server'
 
-export async function GET(){
-	let result
-	try{
-		result = await db.select().from(placeProblems)
-	}catch(e) {
-		return NextResponse.json({
-			success: false,
-			error: e 
-		}, {status: 400})
-	}
+export async function GET() {
+  let result
+  try {
+    result = await db.select().from(placeProblems)
+  } catch (e) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: e,
+      },
+      { status: 400 }
+    )
+  }
 
-	return NextResponse.json({
-		success: true,
-		data: result
-	})
+  return NextResponse.json({
+    success: true,
+    data: result,
+  })
 }
 
 export async function POST(req: Request) {
   const body = await req.json()
 
+  if (!body.placeId || !Array.isArray(body.problemIds)) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: 'placeId e problemIds são obrigatórios',
+      },
+      { status: 400 }
+    )
+  }
+
   try {
     const values = body.problemIds.map((problemId: string) => ({
       placeId: body.placeId,
-      problemId
+      problemId,
     }))
 
     await db.insert(placeProblems).values(values)
 
     return NextResponse.json({
-      success: true
+      success: true,
     })
   } catch (e) {
-    return NextResponse.json({
-      success: false,
-      error: e
-    }, { status: 400 })
+    return NextResponse.json(
+      {
+        success: false,
+        error: e,
+      },
+      { status: 400 }
+    )
   }
 }
 
@@ -50,17 +66,19 @@ export async function DELETE(req: Request) {
       .delete(placeProblems)
       .where(
         and(
-					eq(placeProblems.placeId, body.placeId),
+          eq(placeProblems.placeId, body.placeId),
           eq(placeProblems.problemId, body.problemId)
         )
       )
 
     return NextResponse.json({ success: true })
-
   } catch (e) {
-    return NextResponse.json({
-      success: false,
-      error: e
-    }, { status: 400 })
+    return NextResponse.json(
+      {
+        success: false,
+        error: e,
+      },
+      { status: 400 }
+    )
   }
 }
