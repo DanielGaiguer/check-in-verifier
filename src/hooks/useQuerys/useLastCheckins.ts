1e14dcecfb82cac5fa61a73061221ae257704319
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-04-08 11:42:55 -0300

    fix: bug for checkins useQuery

--- src/hooks/useQuerys/useLastCheckins.ts
+++ src/hooks/useQuerys/useLastCheckins.ts
@@ -15,20 +15,18 @@ interface ApiResponse {
   data: InitialDataCheckin[]
   count: number
 }
-
 export function useLastCheckins() {
-	const { data, isLoading, error } = useQuery<ApiResponse>({
-		queryKey: ['lastCheckins'],
-		queryFn: async () => {
-			const res = await fetch('/api/checkins?range=month')
-			if (!res.ok) throw new Error("Erro ao buscar check-in")
-			const json: ApiResponse = await res.json()
-			const count = json.count
-			return json
-		},
-	})
+  const { data, isLoading, error } = useQuery<ApiResponse>({
+    queryKey: ['lastCheckins'],
+    queryFn: async () => {
+      const res = await fetch('/api/checkins?range=month')
+      if (!res.ok) throw new Error('Erro ao buscar check-in')
+      const json: ApiResponse = await res.json()
+      return json
+    },
+  })
 
-	const checkinData: InitialDataCheckin[] = Array.isArray(data) ? data: []
+  const checkinData: InitialDataCheckin[] = data?.data ?? []
 
-	return { checkinData, checkinsCount: data?.count, isLoading, error}
+  return { checkinData, checkinsCount: data?.count, isLoading, error }
 }
