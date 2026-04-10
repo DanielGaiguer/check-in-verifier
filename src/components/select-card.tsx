4c8c394502f1a234767784f8cec9939c3de652c4
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-04-10 09:28:12 -0300

    feat: add filter for select box, and documentation for tsconfig in development

--- src/components/select-card.tsx
+++ src/components/select-card.tsx
@@ -12,30 +12,40 @@ import {
 import { Dispatch, SetStateAction } from 'react'
 import ErrorPage from './error-page'
 import SelectCardSkeleton from './skeleton-select-card'
+import { toast } from 'react-toastify'
 
 interface selectCardProps {
   textHeader: string
   placeHolder: string
   onChange: Dispatch<SetStateAction<string>>
-  value?: string 
+  value?: string
 }
 
 export default function SelectCard({
   textHeader,
   placeHolder,
   onChange,
-  value
+  value,
 }: selectCardProps) {
   const { people, isLoading, error } = usePeople({ active: true })
 
   if (isLoading) return <SelectCardSkeleton />
   if (error) return <ErrorPage />
 
+
   return (
     <Card className="mt-5">
       <CardHeader>
         <h6 className="text-sm font-semibold">{textHeader}</h6>
-        <Select value={value} onValueChange={(value) => onChange(value)}>
+        <Select
+          value={value}
+          onValueChange={(value) => onChange(value)}
+          onOpenChange={(open) => {
+            if (open && people.length === 0) {
+              toast.error('Nenhuma pessoa cadastrada.')
+            }
+          }}
+        >
           <SelectTrigger className="w-full max-w-80">
             <SelectValue placeholder={placeHolder} className="text-sm" />
           </SelectTrigger>
