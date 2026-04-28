fb9df4e954e4ad237183e242025bc2694218416c
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-04-28 11:56:12 -0300

    fix: bug new Date for parseISO

--- src/components/card-checkins.tsx
+++ src/components/card-checkins.tsx
@@ -4,7 +4,7 @@ import { Card, CardDescription, CardTitle } from './ui/card'
 import { CircleCheckIcon, CircleXIcon } from 'lucide-react'
 import { Badge } from './ui/badge'
 import { Checkin } from '@/types/typesPayload'
-import { format } from 'date-fns'
+import { format, parseISO } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 
 interface CardCheckinProps {
@@ -14,9 +14,9 @@ interface CardCheckinProps {
 export default function CardCheckin({ data }: CardCheckinProps) {
   let status: 'organized' | 'disorganized' | 'not_checked' = 'not_checked'
 
-  if (data.items.every(item => item.status === 'organized')) {
+  if (data.items.every((item) => item.status === 'organized')) {
     status = 'organized'
-  } else if (data.items.some(item => item.status === 'not_checked')) {
+  } else if (data.items.some((item) => item.status === 'not_checked')) {
     status = 'not_checked'
   } else {
     status = 'disorganized'
@@ -29,27 +29,36 @@ export default function CardCheckin({ data }: CardCheckinProps) {
   }
 
   const BADGES = {
-    organized: <Badge className="bg-green-100 text-green-500 font-bold">Tudo OK</Badge>,
-    disorganized: <Badge className="bg-red-100 text-red-500 font-bold">Problemas</Badge>,
-    not_checked: <Badge className="bg-gray-100 text-gray-500 font-bold">Não Verificado</Badge>,
+    organized: (
+      <Badge className="bg-green-100 font-bold text-green-500">Tudo OK</Badge>
+    ),
+    disorganized: (
+      <Badge className="bg-red-100 font-bold text-red-500">Problemas</Badge>
+    ),
+    not_checked: (
+      <Badge className="bg-gray-100 font-bold text-gray-500">
+        Não Verificado
+      </Badge>
+    ),
   }
 
   return (
     <Link href={`/checkins/${data.checkinId}`}>
-      <Card className="hover:shadow-md flex justify-between m-1.5 md:max-h-21.25">
-        <div className='flex justify-between w-full'>
-          <div className="flex ml-5 items-center">
+      <Card className="m-1.5 flex justify-between hover:shadow-md md:max-h-21.25">
+        <div className="flex w-full justify-between">
+          <div className="ml-5 flex items-center">
             {ICONS[status]}
-            <div className='ml-3'>
-              <CardTitle className='mb-1 font-semibold'>{data.people?.name || 'Indefinido'}</CardTitle>
+            <div className="ml-3">
+              <CardTitle className="mb-1 font-semibold">
+                {data.people?.name || 'Indefinido'}
+              </CardTitle>
               <CardDescription>
-                {format(new Date(data.date), 'dd/MM/yyyy', {locale: ptBR})} • {data.placeCount} lugares
+                {format(parseISO(data.date), 'dd/MM/yyyy', { locale: ptBR })} • {' '}
+                {data.placeCount} lugares
               </CardDescription>
             </div>
           </div>
-          <div className='mr-4'>
-            {BADGES[status]}
-          </div>
+          <div className="mr-4">{BADGES[status]}</div>
         </div>
       </Card>
     </Link>
